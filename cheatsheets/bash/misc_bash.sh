a311b42b073c4caf6db3de20c24c1a6c77795bb1
Rafael Magaña <rafael.magana@crowdint.com>
2009-07-28 15:20:50 -0500

    added bash cheatsheets

---
+++ cheatsheets/bash/misc_bash.sh
@@ -0,0 +1,10 @@
+!! run last command
+source .bashrc to not restart the terminal
+
+ls -lSr    #List files (sorted by size)
+df -h      #Show available disk space in human-readable format
+du -sh /some/dir     #Show how much space /some/dir is taking up
+ps aux | grep blah   #List all the running processes but only show ones that contain 'blah'
+wget -spider http://0at.org  #Fetch pages and behave like a web spider: don't download the pages, just check to see if they are there
+ab   #Apache benchmark use this if you want a quick n' dirty way to benchmark how well your site performs under a heavy load.
+perl -i -pe 's/foo/bar/gi' *  #search through * and replace all foo with bar
