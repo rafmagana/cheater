7eb6aa4989994a3bc743b98e8abb5d1ca35d8cb8
Rafael Magaña <rafael.magana@crowdint.com>
2009-07-28 19:55:09 -0500

    added reset

---
+++ cheatsheets/git/reset.sh
@@ -0,0 +1,2 @@
+git reset --hard #it will reset everything in index area and all the not updated files
+git reset --soft HEAD^ #it will undo the last not pushed commit and will let the modified files in index
