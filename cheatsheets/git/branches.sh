66de9fc2e44c7867ad199da7dcab9c64932c2d1d
Rafael Magaña <rafael.magana@crowdint.com>
2009-07-28 19:45:14 -0500

    added branching commands

--- cheatsheets/git/branches.sh
+++ cheatsheets/git/branches.sh
@@ -1,5 +1,6 @@
 git branch $name #Create a new local branch
-git push origin $name #Create remote branch based on a local one
-git branch -d $name #delete a local branch
-git checkout -b $local_branch_name -t $remote_repo/$remote_branch_name #create a local branch a track the remote one
+git push origin $name #Create remote branch or tag based on a local one
+git branch -d|-D $name #delete a local branch
+git checkout -b $local_branch_name -t $remote_repo/$remote_branch_name #create a local branch and track the remote one
+git checkout -t $remote_repo/$remote_branch_name #the same than the previous one (only in mac)
 git push $remote_repo :$branch_or_tag_name #delete a remote branch or a remote tag
