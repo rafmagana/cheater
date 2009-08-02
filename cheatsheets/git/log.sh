851a2c475e84503ded2a1275c441db83a97d6bd1
Rafael Magaña <rafael.magana@crowdint.com>
2009-08-01 23:45:42 -0500

    added more git log stuff

--- cheatsheets/git/log.sh
+++ cheatsheets/git/log.sh
@@ -1,4 +1,6 @@
 git log -M 					#will give the commit history with rename information
 git log -g 					#will show lost commits
 git log $branch1..$branch2 	#different commits between $branch1 and $branch2. ie. "git log local..remote"
-git log <commit>.. $file 	#commits since v2.5 which modify $file
+git log <commit>.. $file 	#commits since <commit> which modify $file
+git log --graph #show the graph in ascii
+git log --pretty=oneline --graph --all #show the graph of all commit, one commit per line
