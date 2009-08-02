a3e3c560cabe935e0a5d4adea7bad631cf84cac4
Rafael Magaña <rafael.magana@crowdint.com>
2009-08-01 23:43:52 -0500

    added bash - run last command and search with perl

--- cheatsheets/bash/misc_bash.sh
+++ cheatsheets/bash/misc_bash.sh
@@ -1,5 +1,5 @@
-!! run last command
-source .bashrc to not restart the terminal
+!! #run last command
+source .bashrc #to not restart the terminal
 
 ls -lSr    #List files (sorted by size)
 df -h      #Show available disk space in human-readable format
@@ -8,3 +8,7 @@ ps aux | grep blah   #List all the running processes but only show ones that con
 wget -spider http://0at.org  #Fetch pages and behave like a web spider: don't download the pages, just check to see if they are there
 ab   #Apache benchmark use this if you want a quick n' dirty way to benchmark how well your site performs under a heavy load.
 perl -i -pe 's/foo/bar/gi' *  #search through * and replace all foo with bar
+
+#split file
+split -b2047m $inputfilename #splits $inputfilename in chunks of 2gb
+cat x* > $completefilename #regenerate the splitted file
