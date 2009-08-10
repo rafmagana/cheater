be224e89b62efb3500f99f5351ffb0c50c3668e4
Rafael Magaña <rafael.magana@crowdint.com>
2009-08-09 19:21:27 -0500

    mysql - added query to look for a column name in all the tables of a db

--- cheatsheets/mysql/misc_mysql.sql
+++ cheatsheets/mysql/misc_mysql.sql
@@ -4,3 +4,7 @@ SHOW innodb STATUS;
 UPDATE [LOW_PRIORITY] [IGNORE] table_references
     SET col_name1={expr1|DEFAULT} [, col_name2={expr2|DEFAULT}] ...
     [WHERE where_condition]
+
+SELECT TABLE_NAME 
+	FROM information_schema.COLUMNS 
+	WHERE TABLE_SCHEMA = ‘dbName’ and COLUMN_NAME like ‘%ColName%’; #search a column name in all the tables
