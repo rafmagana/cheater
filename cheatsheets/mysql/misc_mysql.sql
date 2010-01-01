07b89d1363666ddd18156548c7683ce08e9258a4
Rafael Magaña <rafael.magana@crowdint.com>
2010-01-01 17:46:00 -0600

    added charset and collation to mysql cheatsheet

--- cheatsheets/mysql/misc_mysql.sql
+++ cheatsheets/mysql/misc_mysql.sql
@@ -8,6 +8,26 @@ UPDATE [LOW_PRIORITY] [IGNORE] table_references
 INSERT INTO tbl_name (col1,col2) VALUES(col2*2,15);
 
 mysql -u USER -p DBNAME < dump.sql
+
 SELECT TABLE_NAME 
 	FROM information_schema.COLUMNS 
 	WHERE TABLE_SCHEMA = ‘dbName’ and COLUMN_NAME like ‘%ColName%’; #search a column name in all the tables
+	
+#DELETE
+DROP DATABASE $database_name
+
+#DUMP
+mysqldump -d -h localhost -u root -p2Uad7as9 database01 > dumpfile.sql
+
+#CHARSET AND COLLATION
+ALTER TABLE 'table_name' DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci
+ALTER TABLE 'table_name' CHANGE 'table_name' 'table_name' VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+ALTER TABLE 'table_name' CHANGE 'table_name' 'table_name' VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci
+ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_bin
+
+ALTER DATABASE $database DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+
+#PAGER
+pager less
