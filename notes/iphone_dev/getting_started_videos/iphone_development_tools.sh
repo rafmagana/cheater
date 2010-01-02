3c1abbdb452b39efaa4579ae59689eaccb6bb02c
Rafael Magaña <rafael.magana@crowdint.com>
2010-01-01 21:07:20 -0600

    notes on getting started iphone development videos

---
+++ notes/iphone_dev/getting_started_videos/iphone_development_tools.sh
@@ -0,0 +1,8 @@
+Xcode
+	-- iPhone Management
+		- Organizer
+			• it turns your iPhone to be ready for development
+			• it installs a new version of iPhoneOS
+	-- Code
+		- You can choose to use the simulator or your actual iphone
+		- You can switch the firmwares (std and dev)
