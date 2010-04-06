9ddf51493e18325983cca2abee07f49e66e53ad8
Rafael Magana <raf.magana@gmail.com>
2010-04-06 16:39:21 -0500

    added trick to copy to clipboard from mac terminal

--- cheatsheets/mac/console.sh
+++ cheatsheets/mac/console.sh
@@ -1,3 +1,5 @@
 osascript -e 'tell app "TextMate" to reload bundles'
 
 defaults write com.apple.finder QLEnableXRayFolders 1 #quicklook shows the contents of folder 
+
+cat some_file | pbcopy #copies some_file contents to clipboard
