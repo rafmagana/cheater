66e4a4eb316b2417ca4cf866263006ff85cbdf04
Rafael Magana <raf.magana@gmail.com>
2010-04-06 16:45:35 -0500

    added methods used on ruby metraprogamming

---
+++ notes/ruby/metaprogramming/basics.rb
@@ -0,0 +1,21 @@
+define_method
+method_missing
+method_removed
+method_added
+
+class_eval
+
+instance_eval
+instance_variable_get
+instance_variables_set
+Foo.instance_methods
+
+class << self
+Class.new
+object.send :method
+eval
+
+module_eval
+
+block.call
+block.binding
