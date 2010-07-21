a7889f5df3367c482211ba66965188bc02e5cf00
Rafael Magana <raf.magana@gmail.com>
2010-07-21 17:43:37 -0500

    more mysql things

--- cheatsheets/mysql/misc_mysql.sql
+++ cheatsheets/mysql/misc_mysql.sql
@@ -1,45 +1,12 @@
-#MYSQL MISC QUERIES
-SHOW innodb STATUS;
+http://www.pantz.org/software/mysql/mysqlcommands.html
 
-UPDATE [LOW_PRIORITY] [IGNORE] table_references
-    SET col_name1={expr1|DEFAULT} [, col_name2={expr2|DEFAULT}] ...
-    [WHERE where_condition]
-
-INSERT INTO tbl_name (col1,col2) VALUES(col2*2,15);
-
+mysqlcheck -u root -p –auto-repair –check –optimize –all-databases #issue it every week to improve performance
 mysql -u USER -p DBNAME < dump.sql
-
-SELECT TABLE_NAME 
-	FROM information_schema.COLUMNS 
-	WHERE TABLE_SCHEMA = ‘dbName’ and COLUMN_NAME like ‘%ColName%’; #search a column name in all the tables
 	
-#DELETE
-DROP DATABASE $database_name
-
 #DUMP SCHEMA (structure)
 mysqldump -d -h {host} -u {user} -p {password} {database} > {dumpfile.sql}
 mysqldump -u{user} -p{pass} --skip-triggers --compact --no-create-info {database} > {dumpfile.sql}
 
-
-#CHARSET AND COLLATION
-ALTER TABLE 'table_name' DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci
-ALTER TABLE 'table_name' CHANGE 'table_name' 'table_name' VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
-ALTER TABLE 'table_name' CHANGE 'table_name' 'table_name' VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci
-ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
-ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_bin
-
-ALTER DATABASE $database DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
-
-#PAGER
-pager less
-
-#CREATE USER
-CREATE USER 'user_name'@'server' IDENTIFIED BY 'the_pass';
-
-#PERMISSIONS
-GRANT ALL ON {database}.* TO {user};
-
-http://www.pantz.org/software/mysql/mysqlcommands.html
-
-mysqlcheck -u root -p –auto-repair –check –optimize –all-databases #issue it every week to improve performance
+#import files
+mysql -u root -p -vvf database < file
+mysql -u root -p -vvf < file (si el archivo tiene CREATE )
--- cheatsheets/mysql/queries.sql
+++ cheatsheets/mysql/queries.sql
@@ -1,6 +1,10 @@
 #MYSQL QUERIES
 
 #SELECTING RECORDS
+SELECT TABLE_NAME 
+	FROM information_schema.COLUMNS 
+	WHERE TABLE_SCHEMA = ‘dbName’ and COLUMN_NAME like ‘%ColName%’; #search a column name in all the tables
+
 SELECT * FROM {table} WHERE {field} RLIKE "^regexp$";
 SELECT * FROM {table} WHERE {field} LIKE "%card-igan%";
 SELECT * FROM {table} WHERE {field}={value};
@@ -8,6 +12,10 @@ SELECT * FROM {table};
 select email, count(email) as cnt from accounts group by email having cnt > 1 order by cnt;
 
 #UPDATING RECORDS
+UPDATE [LOW_PRIORITY] [IGNORE] table_references
+    SET col_name1={expr1|DEFAULT} [, col_name2={expr2|DEFAULT}] ...
+    [WHERE where_condition]
+
 UPDATE {table} SET {field}={value} WHERE {field}={value};
 UPDATE twitter_messages SET state="new" WHERE product_id=6114;
 UPDATE $table_name SET $col1 = "new_value" WHERE $record_name = "value";
@@ -21,6 +29,7 @@ TRUNCATE TABLE {table};
 
 #INSERTING RECORDS
 INSERT INTO $table_name VALUES ('MyName', 'MyOwner', '2002-08-31');
+INSERT INTO $table_name (col1,col2) VALUES ("col1", "col2");
 
 #DELETING TABLES
 DROP TABLE {table}
@@ -35,9 +44,24 @@ CREATE USER 'monty'@'localhost' IDENTIFIED BY 'some_pass';
 
 #PERMISSIONS
 SHOW GRANTS FOR 'user_name'@'localhost';
+GRANT ALL ON {database}.* TO {user};
+
+
+#CHARSET AND COLLATION
+ALTER TABLE 'table_name' DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci
+ALTER TABLE 'table_name' CHANGE 'table_name' 'table_name' VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+ALTER TABLE 'table_name' CHANGE 'table_name' 'table_name' VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci
+ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE 'table_name' DEFAULT CHARACTER SET utf8 COLLATE utf8_bin
+
+ALTER DATABASE $database DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
 
 ###Schemas
 RENAME {DATABASE | SCHEMA} db_name TO new_db_name;
 
 ##Gathering information about the tables
 select table_name, table_rows, data_length, index_length from information_schema.partitions where TABLE_SCHEMA={database_name};
+
+#MYSQL MISC QUERIES
+SHOW innodb STATUS;
